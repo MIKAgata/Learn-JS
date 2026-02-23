33d565c1c5853ec0e923b5774ebc0af5b5b7adf9
MIKE <mikael.agata.sitepu@gmail.com>
2026-02-23 23:04:31 +0700

    add function

---
+++ Learn_js/1. Beginner/1/func.js
@@ -0,0 +1,8 @@
+// fungsi merupakan sebuah bagian code yang dapat  melakukan sebuah tindakan dan dapat dipanggil kapan saja tanpa harus mengulang code
+// contoh
+
+function kali(a, b) {
+    return a * b
+}
+
+console.log(kali(12, 2)) //hasilnya akan 24

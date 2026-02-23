c36eda2cc0118032aa065ef09a437d9db49f3940
MIKE <mikael.agata.sitepu@gmail.com>
2026-02-23 23:01:18 +0700

    add code

--- Learn_js/1. Beginner/1/decicion.js
+++ Learn_js/1. Beginner/1/decicion.js
@@ -1,2 +1,10 @@
 // bentarlagi
 // desision merupakan sebuah metode yang berguna untuk membuat percabangan
+// contoh
+let umur = 18
+
+if (umur >= 17) {
+    console.log("kamu sudah bisa membuat KTP")
+} else {
+    console.log("sabar sedikit lagi bro")
+}

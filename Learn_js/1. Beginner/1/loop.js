6c4588f83c57658622b6172d9ef6c846722ddadb
MIKE <mikael.agata.sitepu@gmail.com>
2026-02-28 15:44:23 +0700

    edit loop

--- Learn_js/1. Beginner/1/loop.js
+++ Learn_js/1. Beginner/1/loop.js
@@ -1,4 +1,9 @@
+
+
 let x = 10
- for (let i =1; i < 10; ++i) {
-    console.log(`loop ${i}`)
+ for (let i =1; i <= 10; ++i) {
+   console.log(`loop ${i}`)
+   if (i == 10) {
+      console.log("ini adalah loop")
+   }
  }

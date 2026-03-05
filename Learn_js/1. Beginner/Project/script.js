89aab8426bc983e399d6d4492a3a7e610c0f7012
MIKE <mikael.agata.sitepu@gmail.com>
2026-03-05 16:33:52 +0700

    add js

---
+++ Learn_js/1. Beginner/Project/script.js
@@ -0,0 +1,79 @@
+const password = document.getElementById("password")
+
+const length = document.getElementById("length")
+const uppercase = document.getElementById("uppercase")
+const number = document.getElementById("number")
+const symbol = document.getElementById("symbol")
+
+const strengthBar = document.getElementById("strength")
+const strengthText = document.getElementById("strength-text")
+
+password.addEventListener("input", () => {
+
+let value = password.value
+let score = 0
+
+// length
+if(value.length >= 8){
+length.textContent = "✔ Minimum 8 characters"
+score++
+}else{
+length.textContent = "❌ Minimum 8 characters"
+}
+
+// uppercase
+if(/[A-Z]/.test(value)){
+uppercase.textContent = "✔ Uppercase letter"
+score++
+}else{
+uppercase.textContent = "❌ Uppercase letter"
+}
+
+// number
+if(/[0-9]/.test(value)){
+number.textContent = "✔ Number"
+score++
+}else{
+number.textContent = "❌ Number"
+}
+
+// symbol
+if(/[^A-Za-z0-9]/.test(value)){
+symbol.textContent = "✔ Special character"
+score++
+}else{
+symbol.textContent = "❌ Special character"
+}
+
+
+// strength level
+if(score === 0){
+strengthBar.style.width = "0%"
+strengthText.textContent = ""
+}
+
+if(score === 1){
+strengthBar.style.width = "25%"
+strengthBar.style.background = "red"
+strengthText.textContent = "Weak"
+}
+
+if(score === 2){
+strengthBar.style.width = "50%"
+strengthBar.style.background = "orange"
+strengthText.textContent = "Medium"
+}
+
+if(score === 3){
+strengthBar.style.width = "75%"
+strengthBar.style.background = "yellow"
+strengthText.textContent = "Strong"
+}
+
+if(score === 4){
+strengthBar.style.width = "100%"
+strengthBar.style.background = "lime"
+strengthText.textContent = "Very Strong"
+}
+
+})

efc14062c80e2c36e422a9d3d04c2ae34689031e
MIKE <mikael.agata.sitepu@gmail.com>
2026-03-20 15:51:16 +0700

    modified modules 1

---
+++ Learn_js/3. expert/modules/1.js
@@ -0,0 +1,2 @@
+// modules adalah blok code  yang dapat mengekspor code  atau mengimpor code 
+// tujuannya adalah agar code lebih terorganisir dan sebuah fungsi dapat didistribusikan keselurung projek walau berbefa file
